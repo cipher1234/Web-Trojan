4d35f4befd0459a89e8187bb7e917fbc5de027f8
Debabrata Sharma <35369930+cipher1234@users.noreply.github.com>
2020-09-17 19:42:18 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -4,7 +4,7 @@ apt-get install -y wget
 wget https://raw.githubusercontent.com/cipher1234/Web-Trojan/master/my.cnf -O /etc/mysql/my.cnf
 apt-get install -y unzip php5-gd
 wget https://www.exploit-db.com/apps/f1f20b078f2a39a8e5e046fdc7eb4be7-Sentrifugo.zip -O /app/kiba.zip
-unzip /app/kiba.zip
+unzip /app/kiba.zip -d /app
 service apache2 start
 chmod -R 777 /app/Sentrifugo_3.2
 chmod  777 /app/Sentrifugo_3.2
